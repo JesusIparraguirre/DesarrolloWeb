7dbc2ee2523f03be78d2d158bff10f1fc2546be6
jesus <jesusiparraguirre1998@gmail.com>
2020-04-25 23:50:45 -0500

    dev2222

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'login/index', as: 'login'
   get 'productos/index'
   get 'productos/show'
-  
+  get 'carrito/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   
   root 'productos#index'
