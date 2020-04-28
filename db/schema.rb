1b410c7bc0c730e350ee0317c327bee613fd8bb6
Jesus <jesusiparraguirre1998@gmail.com>
2020-04-28 16:25:11 -0500

    categorias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_195554) do
+ActiveRecord::Schema.define(version: 2020_04_28_063447) do
+
+  create_table "categoria", force: :cascade do |t|
+    t.text "nombre"
+    t.integer "codigo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "nombre"
@@ -29,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_04_21_195554) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
