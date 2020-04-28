f85897b878c8032f6dde1b5b2b4516d16af5f1f1
Jesus <jesusiparraguirre1998@gmail.com>
2020-04-28 17:27:33 -0500

    qwewqeqweqad

--- db/migrate/20200428063447_create_categoria.rb
+++ db/migrate/20200428063447_create_categoria.rb
@@ -2,7 +2,7 @@ class CreateCategoria < ActiveRecord::Migration[6.0]
   def change
     create_table :categoria do |t|
       t.text :nombre
-      t.integer :codigo
+      t.integer :codigos
 
       t.timestamps
     end
