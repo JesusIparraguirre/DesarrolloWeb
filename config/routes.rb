607c5ff559c3348647c6e3555003daccef3fd5a1
Jesus <jesusiparraguirre1998@gmail.com>
2020-04-28 22:44:46 -0500

    ADMINISTRADOR_VISTA

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources:productos
   get 'login/index', as: 'login'
+
   get 'productos/index'
   get 'productos/show'
+
   get 'carrito/index'
+
+  get 'administrador/index'
+  get 'administrador/stock'
+  get 'administrador/producto'
+  get 'administrador/codigo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   
   root 'productos#index'
