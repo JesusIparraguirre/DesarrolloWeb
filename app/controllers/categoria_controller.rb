009eeb544619139c0e773587d121ef7d08ce8c9f
Jesus <jesusiparraguirre1998@gmail.com>
2020-04-29 10:36:06 -0500

    q

--- app/controllers/categoria_controller.rb
+++ app/controllers/categoria_controller.rb
@@ -1,2 +1,8 @@
 class CategoriaController < ApplicationController
+    def show
+        @categoria = Categorium.find(params[:id])
+        #@productos = Product.id(params[:id])
+        @productos = Product.all
+        @categorias = Categorium.all
+    end
 end
